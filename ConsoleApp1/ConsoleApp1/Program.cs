ddfa2ffb9b35579124644d51476dd8f44c15834a
juanpabloramosp <juanpabloramosp@hotmail.com>
2021-04-05 09:09:54 -0600

    Corrección Funcionalidad

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-            //Cambio en funcionalidad principoal
+            //Cambio en funcionalidad principal
         }
     }
 }
